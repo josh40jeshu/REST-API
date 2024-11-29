806bb0cb33f7c86e596e000dd4ea2ef3b0de4beb
josh40jeshu <166501153+josh40jeshu@users.noreply.github.com>
2024-11-29 17:24:54 +0000

    commit

--- client.py
+++ client.py
@@ -2,3 +2,11 @@ import requests
 import json
 
 BASE_URL = 'http://localhost:5000'
+
+def add_product(name, description, price):
+    payload = {
+        'name': name,
+        'description': description,
+        'price': price
+    }
+    
